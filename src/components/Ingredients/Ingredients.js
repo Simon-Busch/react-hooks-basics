81942266bdfa3ad760a23ac10eabc18e76661f06
Simon Busch <simon.busch@live.fr>
2021-01-17 11:12:12 +0100

    add loading

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -6,6 +6,7 @@ import Search from './Search';
 
 const Ingredients = () => {
   const [ userIngredients, setUserIngredients ] = useState([]);
+  const[isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     console.log('rendering useEffect', userIngredients)
@@ -17,12 +18,14 @@ const Ingredients = () => {
   //here no dependencies
 
   const addIngredientHandler = (ingredient) => {
+    setIsLoading(true);
     fetch('https://react-hook-test-9bdb4-default-rtdb.firebaseio.com/ingredients.json', {
       method:'POST',
       body: JSON.stringify(ingredient),
       headers: { 'Content-Type': 'application/json' }
     }).then(response => {
-        return response.json();
+      setIsLoading(false);
+      return response.json();
     }).then(data => {
       setUserIngredients(prevIngredients => [
         ...prevIngredients, 
@@ -33,12 +36,20 @@ const Ingredients = () => {
   }
 
   const removeIngredientHandler = ingId => {
-    setUserIngredients(prevIngredients => prevIngredients.filter((ingredient) => ingredient.id !== ingId))
+    setIsLoading(true);
+    //you need to specify in the url which item you want to delete to send the request
+    fetch(`https://react-hook-test-9bdb4-default-rtdb.firebaseio.com/ingredients/${ingId}.json`, {
+      method:'DELETE'
+    })
+    .then(response => {
+      setIsLoading(false);
+      setUserIngredients(prevIngredients => prevIngredients.filter((ingredient) => ingredient.id !== ingId))
+    });
   }
 
   return (
     <div className="App">
-      <IngredientForm  onAddIngredient={addIngredientHandler} />
+      <IngredientForm  onAddIngredient={addIngredientHandler} loading={isLoading}/>
 
       <section>
         <Search onLoadIngredients={filteredIngrediensHandler} />
--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 import Card from '../UI/Card';
 import './Search.css';
@@ -6,23 +6,40 @@ import './Search.css';
 const Search = React.memo(props => {
   const [enteredFilter, setEnteredFilter ] = useState('');
   const { onLoadIngredients } = props; 
+  const inputRef = useRef()
 
   useEffect(() => {
-    const query = enteredFilter.length === 0 ? '' : `?orderBy="title"&equalTo="${enteredFilter}"`;
-    fetch('https://react-hook-test-9bdb4-default-rtdb.firebaseio.com/ingredients.json' + query)
-    .then(response => response.json())
-    .then(responseData => {
-      const loadedIngredients = [];
-      for (const key in responseData) {
-        loadedIngredients.push({
-          id: key,
-          title: responseData[key].title,
-          amount: responseData[key].amount
+    const timer = setTimeout(() => {
+      //thanks to CLOSURE, enteredFilter here is the value that was entered 500ms ago
+      // not the initial value
+      //we need to be able to check with the current value now 
+      // done with REF
+      if (enteredFilter ===  inputRef.current.value) {
+        const query = enteredFilter.length === 0 ? '' : `?orderBy="title"&equalTo="${enteredFilter}"`;
+        fetch('https://react-hook-test-9bdb4-default-rtdb.firebaseio.com/ingredients.json' + query)
+        .then(response => response.json())
+        .then(responseData => {
+          const loadedIngredients = [];
+          for (const key in responseData) {
+            loadedIngredients.push({
+              id: key,
+              title: responseData[key].title,
+              amount: responseData[key].amount
+            });
+          };
+          onLoadIngredients(loadedIngredients);
         });
       }
-      onLoadIngredients(loadedIngredients);
-    });
-  }, [enteredFilter, onLoadIngredients]);
+    }, 500);
+
+    return () => {
+      clearTimeout(timer)
+    };
+    // will make that we clear the timeout
+    //because we don't want to keep the old timer for every keystroke
+
+    //use setTimeout so the function doesn't trigger on everykeystroke but only if the user stops for 500ms
+  }, [enteredFilter, onLoadIngredients, inputRef]);
   // in the array, we need to specific our dependencies
   //function will happen only when enteredFilter change 
 
@@ -32,6 +49,7 @@ const Search = React.memo(props => {
         <div className="search-input">
           <label>Filter by Title</label>
           <input 
+            ref={inputRef}
             type="text" 
             value={enteredFilter}
             onChange={event => setEnteredFilter(event.target.value)}
